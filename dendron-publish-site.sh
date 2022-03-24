6ab74de190cfdab42dcf032b176f8c0e56fc65a0
Huy <86811584+h7b@users.noreply.github.com>
2022-03-25 00:52:04 +0100

    Update dendron-publish-site.sh
    Update dendron-publish-site.sh following changes in https://github.com/dendronhq/template.publish.netlify/

--- dendron-publish-site.sh
+++ dendron-publish-site.sh
@@ -2,22 +2,27 @@
 # dendron-publish-site.sh
 # This script should exist in the root of your workspace
 
-# Remove cached or otherwise present directories meant to be replaced
-rm -rf .next
+# Remove docs directory if present
 rm -rf docs
-rm -rf node_modules
+
+# Uncomment if wishing to remove cache
+# Recommended: Leave cache alone to increase speed of deployments
+#rm -rf .next
+#rm -rf node_modules
 
 # Install latest version of Dendron
-npm install @dendronhq/dendron-cli@latest
+# yarn add @dendronhq/dendron-cli@latest
 # OPTIONALLY
-# Install version of Dendron from package-lock.json in workspace root
+# Install version of Dendron from yarn.lock in workspace root
 # To use:
-# Uncomment the next line, and comment out the other `npm install ...` line
-# npm install
+# Uncomment the next line, and comment out the other `yarn install ...` line
+yarn
+
+# Update Dendron Next.js if needed
+(test -d .next) && (echo 'updating dendron next...' && cd .next && git reset --hard && git pull && yarn && cd ..) || (echo 'init dendron next' && yarn dendron publish init)
 
 # Generate static site with nextjs
-npx dendron publish init
-npx dendron publish export
+yarn dendron publish export
 
 # Move generated website to docs directory in workspace root
 mv .next/out docs
